c2774b94f4eb3f0173db3df03bc55b1999939a1f
yuhcee <egbouchenna1@gmail.com>
2021-04-30 17:32:16 +0100

    initial commit🚀

---
+++ README.md
@@ -0,0 +1,7 @@
+# YuhceeGram
+
+ A sample picture sharing app that models instagram features. 
+
+This is the next big thang mehn...!🔥🔥🔥
+
+Going to the moon soon🚀🚀🚀
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome To YuhceeGram</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// Client Side 
